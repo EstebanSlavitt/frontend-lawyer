a7852fa14216bfe3c5eafb74640b48ccd7477e38
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-09-30 10:39:23 -0700

    intianl commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { LawyersPage } from "./LawyersPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LawyersPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LawyersPage.jsx
@@ -0,0 +1,7 @@
+export function LawyersPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  );
+}
