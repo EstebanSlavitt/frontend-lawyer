0f3760275779b7b0a53c1119af1ddd2494897778
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-09-30 12:43:52 -0700

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
-import { LawyersPage } from "./LawyersPage";
 import { Footer } from "./Footer";
+import { LawyersPage } from "./LawyersPage";
+import { SignupPage } from "./SignupPage"; // Add if this exists
+import { LoginPage } from "./LoginPage"; // Add if this exists
+
+// Define routes
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <LawyersPage />, // Main page with lawyers
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />, // Sign up page
+      },
+      {
+        path: "/login",
+        element: <LoginPage />, // Login page
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <LawyersPage />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
