9e04ee32f0ba7470ddf59d5fc732264963599807
Esteban Slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2025-07-19 11:58:05 -0700

    Remove Vite base for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-lawyer/', // use your repo name here
   plugins: [react()],
-})
+});
